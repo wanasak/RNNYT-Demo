938a9e8968e9cd128138539d50d4479fb10b00cf
Wanasak <u510610433@gmail.com>
2017-12-24 23:31:42 +0700

    ImagePickerIOS and RCTCameraRoll linking

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    ImagePickerIOS,
+    Image,
+    TouchableOpacity
 } from "react-native";
 import Icon from "react-native-vector-icons/EvilIcons";
 
@@ -11,19 +14,58 @@ import AppText from "../components/AppText";
 import * as globalStyles from "../styles/global";
 
 export default class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { image: null };
+
+        this.onPressIcon = this.onPressIcon.bind(this);
+    }
+
+    onPressIcon() {
+        ImagePickerIOS.openSelectDialog(
+            {},
+            imageUri => {
+                this.setState({ image: imageUri });
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
     render() {
         return (
-            <View style={[globalStyles.COMMON_STYLES.pageContainer, styles.container]}>
-                <Icon 
-                    name="user"
-                    style={styles.avartarIcon}
-                />
+            <View
+                style={[
+                    globalStyles.COMMON_STYLES.pageContainer,
+                    styles.container
+                ]}
+            >
+                {this.state.image ? (
+                    <View style={{ paddingBottom: 20 }}>
+                        <TouchableOpacity
+                            onPress={this.onPressIcon}
+                        >
+                            <Image
+                                style={styles.image}
+                                source={{ uri: this.state.image }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                    <Icon
+                        name="user"
+                        style={styles.avartarIcon}
+                        onPress={this.onPressIcon}
+                    />
+                )}
                 <Title>Username</Title>
                 <AppText>Your Name</AppText>
             </View>
         );
     }
-};
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -33,5 +75,9 @@ const styles = StyleSheet.create({
     avartarIcon: {
         color: globalStyles.HEADER_TEXT_COLOR,
         fontSize: 200
+    },
+    image: {
+        width: 150,
+        height: 150
     }
 });
